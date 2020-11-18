7b55cb5ad0a6704eb59e4b2bf6be93646143e4b3
MarcinBalejko <mbbm9@o2.pl>
2020-11-18 12:25:51 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { Fragment } from "react";
+import "./App.css";
+
+const App = (props) => {
+  return (
+    <Fragment>
+      <p>Hello World</p>
+    </Fragment>
+  );
+};
+
+export default App;
